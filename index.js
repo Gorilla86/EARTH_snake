c75767ded7d6b3c02c51e6316543d467dc014f9c
Gorilla86 <86343632+Gorilla86@users.noreply.github.com>
2021-07-06 14:20:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ bot.on("callback_query", function (query) {
         let gameurl: = "t.me/EARTHsnakeBot", id="+query.id;  // add url here
         bot.answerCallbackQuery({
             callback_query_id: query.id,
-      url:(t.me/EARTHsnakeBot)
+      url:(https://git.heroku.com/earthsnake.git)
     });
   }
 });
